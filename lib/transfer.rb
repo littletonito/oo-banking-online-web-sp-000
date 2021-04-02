ce8ae23b63fcc4d0d9da9c7702bd624a2c50b4b7
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-02 19:44:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,15 @@
 # class Transfer
-# 
+#
 #   attr_accessor  :status
 #   attr_reader :sender, :receiver, :amount
-# 
+#
 #   def initialize(sender, receiver, amount)
 #     @sender = sender
 #     @receiver = receiver
 #     @amount = amount
 #     @status = "pending"
 #   end
-# 
+#
 #   def valid?
 #     if sender.valid? && receiver.valid?
 #       true
@@ -17,7 +17,7 @@
 #       false
 #     end
 #   end
-# 
+#
 #   def execute_transaction
 #     if valid? && sender.balance > amount && self.status == "complete"
 #       sender.balance -= amount
@@ -27,7 +27,7 @@
 #       reject_transfer
 #     end
 #   end
-# 
+#
 #   def reverse_transfer
 #     if valid? && receiver.balance > amount && self.status == "complete"
 #       receiver.balance -= amount
@@ -37,12 +37,12 @@
 #       reject_transfer
 #     end
 #   end
-# 
+#
 #   def reject_transfer
 #     self.status = "rejected"
 #     "Transaction rejected. Please check your account balance."
 #   end
-# 
+#
 # end
 
 
