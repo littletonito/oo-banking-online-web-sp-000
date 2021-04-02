5b4c3236e35330d863eb4b6f1fc100aafdb6cdee
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-02 19:30:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,17 @@ class Transfer
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
     end
-    
+
+    def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
   end
 
 
