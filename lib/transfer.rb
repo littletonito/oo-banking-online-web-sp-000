ea3d63a25b287e1c1ccf7048f75af0afbad80505
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-02 19:28:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance > amount && self.status == "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status == "complete"
