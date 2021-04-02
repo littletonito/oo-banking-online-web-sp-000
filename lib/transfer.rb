5da78933363186a7af58a8f07173d55e691bf23f
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-02 19:36:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
 
   attr_accessor  :status 
   attr_reader :sender, :receiver, :amount
-  
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
